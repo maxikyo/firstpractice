86c1448480395fb6a617b2def41140a9904b3aa6
Max <holovanovmax@gmail.comgit config --local user.email>
2022-06-06 18:12:07 +0200

    first commit

---
+++ example/js/script.js
@@ -0,0 +1,21 @@
+console.log('arr' + " - object");
+// console.log(4 + +"5"); //second + - унарный
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+//
+// console.log(5%2); // деление.
+//
+ console.log(2 + 2 * 2 !== 8);
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);
+
